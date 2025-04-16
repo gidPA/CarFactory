04262c8be41237f749f6feccb32c98a7fe4a4c38
SE-86\SE-86 <gideon.purwoko@m3.formulatrix.com>
2025-04-16 16:34:42 +0700

    added User Interaction

--- CarFactory/Factory.cs
+++ CarFactory/Factory.cs
@@ -1,4 +1,5 @@
-enum CarType{
+enum CarType
+{
     EV,
     ICE,
     Hybrid
@@ -17,66 +18,97 @@ interface IInternalCombustionVehicle
 
 class Car
 {
-    public virtual void Drive(){
+    public virtual void Drive()
+    {
 
     }
-    public void Honk(){
+    public void Honk()
+    {
         Console.WriteLine("Honk Honk!");
     }
 }
 
 static class CarFactory
 {
-    class RealCarICE: Car, IInternalCombustionVehicle{
+    class RealCarICE : Car, IInternalCombustionVehicle
+    {
         private int FuelPercentage = 100;
 
-        public void Refuel(){
+        public void Refuel()
+        {
             FuelPercentage = 100;
             Console.WriteLine("Your car is now refueled");
         }
 
-        public override void Drive(){
-            FuelPercentage -= 25;
-            Console.WriteLine("Vroom....");
-            Console.WriteLine("Your Fuel Percentage is: {0}", FuelPercentage);
-        }
-    }
-
-    class RealCarEV: Car, IElectricVehicle{
-        private int BatteryPercentage = 100;
-
-        public void Recharge(){
-            BatteryPercentage = 100;
-            Console.WriteLine("Your car is now fully charged");
-        }
-
-        public override void Drive(){
-            BatteryPercentage -= 25;
-            Console.WriteLine("Woosh....");
-            Console.WriteLine("Your Battery Percentage is: {0}", BatteryPercentage);
-        }
-    }
-
-    class RealCarHybrid: Car, IElectricVehicle, IInternalCombustionVehicle{
-        private int FuelPercentage = 100;
-        private int BatteryPercentage = 100;
-
-        public void Refuel(){
-            Console.WriteLine("Your car is now refueled");
-            FuelPercentage = 100;
-        }
-
-        public void Recharge(){
-            Console.WriteLine("Your car is now fully charged");
-            BatteryPercentage = 100;
-        }
-
-        public override void Drive(){
-            if(BatteryPercentage < 25){
+        public override void Drive()
+        {
+            if(FuelPercentage <= 0){
+                Console.WriteLine("You're running out of fuel");
+            } else {
                 FuelPercentage -= 25;
                 Console.WriteLine("Vroom....");
                 Console.WriteLine("Your Fuel Percentage is: {0}", FuelPercentage);
-            } else {
+            }
+
+        }
+    }
+
+    class RealCarEV : Car, IElectricVehicle
+    {
+        private int BatteryPercentage = 100;
+
+        public void Recharge()
+        {
+            BatteryPercentage = 100;
+            Console.WriteLine("Your car is now fully charged");
+        }
+
+        public override void Drive()
+        {
+            if (BatteryPercentage <= 0)
+            {
+                Console.WriteLine("You're running out of battery power.");
+            }
+            else
+            {
+                BatteryPercentage -= 25;
+                Console.WriteLine("Woosh....");
+                Console.WriteLine("Your Battery Percentage is: {0}", BatteryPercentage);
+            }
+
+        }
+    }
+
+    class RealCarHybrid : Car, IElectricVehicle, IInternalCombustionVehicle
+    {
+        private int FuelPercentage = 100;
+        private int BatteryPercentage = 100;
+
+        public void Refuel()
+        {
+            Console.WriteLine("Your car is now refueled");
+            FuelPercentage = 100;
+        }
+
+        public void Recharge()
+        {
+            Console.WriteLine("Your car is now fully charged");
+            BatteryPercentage = 100;
+        }
+
+        public override void Drive()
+        {
+            if(BatteryPercentage <= 0 && FuelPercentage <= 0 ){
+                Console.WriteLine("You're running out of fuel and battery power");
+            }
+            else if (BatteryPercentage <= 0)
+            {
+                FuelPercentage -= 25;
+                Console.WriteLine("Vroom....");
+                Console.WriteLine("Your Fuel Percentage is: {0}", FuelPercentage);
+            }
+            else
+            {
                 BatteryPercentage -= 25;
                 Console.WriteLine("Woosh....");
                 Console.WriteLine("Your Battery Percentage is: {0}", BatteryPercentage);
@@ -84,14 +116,15 @@ static class CarFactory
         }
     }
 
-    public static Car? GetCar(CarType type){
+    public static Car? GetCar(CarType type)
+    {
         return type switch
-            {
-                CarType.ICE => new RealCarICE(),
-                CarType.EV => new RealCarEV(),
-                CarType.Hybrid => new RealCarHybrid(),
-                _ => null,
-            };
+        {
+            CarType.ICE => new RealCarICE(),
+            CarType.EV => new RealCarEV(),
+            CarType.Hybrid => new RealCarHybrid(),
+            _ => null,
+        };
 
     }
 
--- CarFactory/Program.cs
+++ CarFactory/Program.cs
@@ -1,22 +1,106 @@
-﻿class Program{
-    public static void Main(){
-        Car myNewCar = CarFactory.GetCar(CarType.Hybrid);
+﻿class Program
+{
+    public static Car? myCar;
+    public static void Main()
+    {
+        Console.WriteLine("GPA's CLI car factory simulator \nFor simulating the concept of interfaces\n\n");
 
-        myNewCar.Drive();
-        myNewCar.Drive();
-        myNewCar.Drive();
-        myNewCar.Drive();
-        myNewCar.Drive();
-        myNewCar.Honk();
-        SendCarToChargingStation((IElectricVehicle) myNewCar);
-        myNewCar.Drive();
+        while (true)
+        {
+            ConsoleKeyInfo option;
+            Console.WriteLine
+            (
+                "\nSelect an action: \n" +
+                "1. Get a new Car \n" +
+                "2. Drive your car \n" +
+                "3. Refuel/Recharge your car \n" +
+                "4. Quit"
+            );
+
+            option = Console.ReadKey(intercept: true);
+
+            if (option.Key == ConsoleKey.D1)
+            {
+                GetNewCarUI();
+            }
+            else if (option.Key == ConsoleKey.D2)
+            {
+                DriveCarUI();
+            }
+            else if (option.Key == ConsoleKey.D3)
+            {
+                RefreshCarUI();
+            }
+            else if (option.Key == ConsoleKey.D4)
+            {
+                break;
+            }
+        }
+
     }
 
-    public static void SendCarToChargingStation(IElectricVehicle EV){
+    public static void GetNewCarUI()
+    {
+        Console.WriteLine
+        (
+            "\nSelect your car type: \n" +
+            "1. Internal Combustion Vehicle \n" +
+            "2. Electric Vehicle \n" +
+            "3. Hybrid Vehicle"
+        );
+        ConsoleKeyInfo option = Console.ReadKey(intercept: true);
+
+        switch (option.Key)
+        {
+            case ConsoleKey.D1:
+                Console.WriteLine("Internal Combustion Vehicle Selected \n");
+                myCar = CarFactory.GetCar(CarType.ICE);
+                break;
+            case ConsoleKey.D2:
+                Console.WriteLine("Electric Vehicle Selected \n");
+                myCar = CarFactory.GetCar(CarType.EV);
+                break;
+            case ConsoleKey.D3:
+                Console.WriteLine("Hybrid Vehicle Selected \n");
+                myCar = CarFactory.GetCar(CarType.Hybrid);
+                break;
+        }
+    }
+
+    public static void DriveCarUI()
+    {
+        if (myCar is null)
+        {
+            Console.WriteLine("You have not picked your car yet");
+        }
+        else
+        {
+            myCar.Drive();
+        }
+    }
+
+    public static void RefreshCarUI()
+    {
+        if (myCar is null)
+        {
+            Console.WriteLine("You have not picked your car yet");
+        }
+        else if (myCar is IElectricVehicle vehicle)
+        {
+            SendCarToChargingStation(vehicle);
+        }
+        else if (myCar is IInternalCombustionVehicle vehicle1)
+        {
+            SendCarToGasStation(vehicle1);
+        }
+    }
+    public static void SendCarToChargingStation(IElectricVehicle EV)
+    {
         EV.Recharge();
     }
 
-    public static void SendCarToGasStation(IInternalCombustionVehicle ICE){
+    public static void SendCarToGasStation(IInternalCombustionVehicle ICE)
+    {
         ICE.Refuel();
     }
 }
