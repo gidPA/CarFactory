35694d774b78aaeaf68c207116b4efdd1eb581d6
SE-86\SE-86 <gideon.purwoko@m3.formulatrix.com>
2025-04-16 14:31:43 +0700

    initial commit

---
+++ CarFactory/Factory.cs
@@ -0,0 +1,98 @@
+enum CarType{
+    EV,
+    ICE,
+    Hybrid
+}
+
+interface IElectricVehicle
+{
+
+    void Recharge();
+}
+
+interface IInternalCombustionVehicle
+{
+    void Refuel();
+}
+
+class Car
+{
+    public virtual void Drive(){
+
+    }
+    public void Honk(){
+        Console.WriteLine("Honk Honk!");
+    }
+}
+
+static class CarFactory
+{
+    class RealCarICE: Car, IInternalCombustionVehicle{
+        private int FuelPercentage = 100;
+
+        public void Refuel(){
+            FuelPercentage = 100;
+            Console.WriteLine("Your car is now refueled");
+        }
+
+        public override void Drive(){
+            FuelPercentage -= 25;
+            Console.WriteLine("Vroom....");
+            Console.WriteLine("Your Fuel Percentage is: {0}", FuelPercentage);
+        }
+    }
+
+    class RealCarEV: Car, IElectricVehicle{
+        private int BatteryPercentage = 100;
+
+        public void Recharge(){
+            BatteryPercentage = 100;
+            Console.WriteLine("Your car is now fully charged");
+        }
+
+        public override void Drive(){
+            BatteryPercentage -= 25;
+            Console.WriteLine("Woosh....");
+            Console.WriteLine("Your Battery Percentage is: {0}", BatteryPercentage);
+        }
+    }
+
+    class RealCarHybrid: Car, IElectricVehicle, IInternalCombustionVehicle{
+        private int FuelPercentage = 100;
+        private int BatteryPercentage = 100;
+
+        public void Refuel(){
+            Console.WriteLine("Your car is now refueled");
+            FuelPercentage = 100;
+        }
+
+        public void Recharge(){
+            Console.WriteLine("Your car is now fully charged");
+            BatteryPercentage = 100;
+        }
+
+        public override void Drive(){
+            if(BatteryPercentage < 25){
+                FuelPercentage -= 25;
+                Console.WriteLine("Vroom....");
+                Console.WriteLine("Your Fuel Percentage is: {0}", FuelPercentage);
+            } else {
+                BatteryPercentage -= 25;
+                Console.WriteLine("Woosh....");
+                Console.WriteLine("Your Battery Percentage is: {0}", BatteryPercentage);
+            }
+        }
+    }
+
+    public static Car? GetCar(CarType type){
+        return type switch
+            {
+                CarType.ICE => new RealCarICE(),
+                CarType.EV => new RealCarEV(),
+                CarType.Hybrid => new RealCarHybrid(),
+                _ => null,
+            };
+
+    }
+
+}
---
+++ CarFactory/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program{
+    public static void Main(){
+        Car myNewCar = CarFactory.GetCar(CarType.Hybrid);
+
+        myNewCar.Drive();
+        myNewCar.Drive();
+        myNewCar.Drive();
+        myNewCar.Drive();
+        myNewCar.Drive();
+        myNewCar.Honk();
+        SendCarToChargingStation((IElectricVehicle) myNewCar);
+        myNewCar.Drive();
+    }
+
+    public static void SendCarToChargingStation(IElectricVehicle EV){
+        EV.Recharge();
+    }
+
+    public static void SendCarToGasStation(IInternalCombustionVehicle ICE){
+        ICE.Refuel();
+    }
+}
